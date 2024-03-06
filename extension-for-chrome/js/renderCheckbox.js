56ab2bf09f4ba01c215aa713fed2175e9419dc3e
MishynMykhailo <misham18092001@gmail.com>
2024-03-06 23:59:17 +0200

    Update renderCheckbox.js

--- extension-for-chrome/js/renderCheckbox.js
+++ extension-for-chrome/js/renderCheckbox.js
@@ -24,60 +24,67 @@
             flex-direction: row;
                     background-color:white;
         width:100%;
-        padding:5px
+        padding:5px;
+        font-size:18px;
         }
-      .custom__resendLeads-label {
-        position: relative;
-        cursor: pointer;
-        font-weight:600;
-        font-size:13px;
-        text-align: center;
-    align-items: center;
-    display: flex;
-    justify-content: center;
+        .custom__resendLeads-label{
+            color:black;
+            margin-left:10px;
+            width:100%;
+            text-align:left;
+        }
+    //   .custom__resendLeads-label {
+    //     position: relative;
+    //     cursor: pointer;
+    //     font-weight:600;
+    //     font-size:13px;
+    //     text-align: center;
+    // align-items: center;
+    // display: flex;
+    // justify-content: center;
 
-      }
-      .custom__resendLeads-label:before {
-        content: "";
-        background-color: transparent;
-        border: 2px solid #0079bf;
-        box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05),
-          inset 0px -15px 10px -12px rgba(0, 0, 0, 0.05);
-        width: 20px;
-        height: 20px;
-        display: inline-block;
-        position: relative;
-        vertical-align: middle;
-        cursor: pointer;
-        margin-right: 5px;
-        transition: 250ms all ease;
-      }
-      .custom__resendLeads-label__checkbox {
-        padding: 0;
-        height: initial;
-        width: initial;
-        margin-bottom: 0;
-        display: none;
-        cursor: pointer;
-        transition: 250ms all ease;
-      }
-      .custom__resendLeads-label__checkbox:checked
-        + .custom__resendLeads-label:after {
-        content: "✓";
-        display: block;
-        position: absolute;
-        top: -3px;
-        left: 4px;
-        color: white;
-        transition: 250ms all ease;
-        font-size:16px!important;
+    //   }
+    //   .custom__resendLeads-label:before {
+    //     content: "";
+    //     background-color: transparent;
+    //     border: 2px solid #0079bf;
+    //     box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05),
+    //       inset 0px -15px 10px -12px rgba(0, 0, 0, 0.05);
+    //     width: 20px;
+    //     height: 20px;
+    //     display: inline-block;
+    //     position: relative;
+    //     vertical-align: middle;
+    //     cursor: pointer;
+    //     margin-right: 5px;
+    //     transition: 250ms all ease;
+    //   }
+    //   .custom__resendLeads-label__checkbox {
+    //     padding: 0;
+    //     height: initial;
+    //     width: initial;
+    //     margin-bottom: 0;
+    //     display: none;
+    //     cursor: pointer;
+    //     transition: 250ms all ease;
+    //   }
+    //   .custom__resendLeads-label__checkbox:checked
+    //     + .custom__resendLeads-label:after {
+    //     content: "✓";
+    //     display: block;
+    //     position: absolute;
+    //     top: -3px;
+    //     left: 4px;
+    //     color: white;
+    //     transition: 250ms all ease;
+    //     font-size:16px!important;
 
-      }
-      .custom__resendLeads-label__checkbox:checked
-        + .custom__resendLeads-label:before {
-        background-color: #0079bf;
-        transition: 250ms all ease;
-      }
+    //   }
+    //   .custom__resendLeads-label__checkbox:checked
+    //     + .custom__resendLeads-label:before {
+    //     background-color: #0079bf;
+    //     transition: 250ms all ease;
+    //   }
     `;
         currentForm.prepend(style);
         const divForInput = document.createElement("div");
